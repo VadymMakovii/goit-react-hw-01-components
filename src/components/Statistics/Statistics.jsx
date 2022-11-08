a970e50ccb4683a2924ee876c21095a7ad54071b
VadymMakovii <makovii.vadym@gmail.com>
2022-11-08 23:38:00 +0200

    Completed task 1-3

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({title, stats}) => {
+  return (
+    <section className={css.statistics}>
+      {title && (<h2 className={css.title}>{title}</h2>)}
+          <ul className={css["stat-list"]}>
+        {stats.map((item) => (
+        <li key={item.id} className={css.item} style={{backgroundColor: getRandomHexColor()}}>
+          <span className={css.label}>{item.label}</span>
+          <span className={css.percentage}>{item.percentage}%</span>
+        </li>
+              ))}
+      </ul>
+    </section>
+  );
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  })),
+};
